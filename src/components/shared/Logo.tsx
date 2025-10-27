af204f4718e53417f870266df377e6d9692e585d
Josue Mugangu <josuemugangu2005@gmail.com>
2025-10-27 15:40:40 +0000

    Pourquoi le logo ou l'image ne s'affiche pas?

--- src/components/shared/Logo.tsx
+++ src/components/shared/Logo.tsx
@@ -1,17 +1,11 @@
 import Link from 'next/link';
-import Image from 'next/image';
 
 const Logo = () => {
   return (
     <Link href="/">
-      <Image
-        src="/logo.png"
-        alt="Kujuwa Consulting Logo"
-        width={150}
-        height={62}
-        priority
-        className="w-auto h-auto"
-      />
+      <div className="font-headline text-2xl font-bold text-foreground">
+        Kujuwa Consulting
+      </div>
     </Link>
   );
 };
